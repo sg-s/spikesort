3613ad31a8a4ac05e743af077ca4599539fe9795
srinivas.gs <github@srinivas.gs>
2016-11-07 16:23:38 -0500

    worked on making read-data a plugin

--- @spikesort/readData_kontroller.m
+++ @spikesort/readData_kontroller.m
@@ -4,7 +4,10 @@
 % 
 function s = readData_kontroller(s,src,event)
 
-% load the data requested
+% read the voltage trace for the current file
 m = matfile([s.path_name s.file_name]);
 this_data = m.data(1,s.this_paradigm);
 s.raw_voltage = this_data.voltage(s.this_trial,:);
+
+
+% read the stimulus trace for the current file for the current trial 
